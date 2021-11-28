961306f9bc169872f4b5cad165ecb93b616cbc49
RomDmitriy <55810251+RomDmitriy@users.noreply.github.com>
2021-11-28 13:20:21 +0300

    Удаление шаблона private-info.js

--- private-info.js
+++
@@ -1,9 +0,0 @@
-//используется для создания access token
-export const jwt_key = "jwt_key";
-
-//авторизация в базу данных Postgresql
-export const db_ip = "your_database_ip";
-export const db_user = "your_database_username";
-export const db_pass = "your_database_password";
-export const db_port = your_database_port;
-export const db_name = "your_database_name";
